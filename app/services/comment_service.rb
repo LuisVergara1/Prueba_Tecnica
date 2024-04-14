0dca0c68d57c4efd127c2feda5f28a7e5f550927
LuisVergara1 <luismarcelo.v.c@hotmail.com>
2024-04-14 16:58:16 -0400

    Modificacion de Cors y Servicio de Comentarios

--- app/services/comment_service.rb
+++ app/services/comment_service.rb
@@ -1,12 +1,12 @@
 class CommentService
   def self.get_all
-    comments = Comment.all.map { |comment| comment.as_json(only: [:id, :body]) }
+    comments = Comment.all.map { |comment| comment.as_json(only: [:id, :body,:earthquake_id]) }
     comments
   end
 
   def self.get_by_id(feature_id)
     feature = Earthquake.find(feature_id)
-    comments = feature.comments.map { |comment| comment.as_json(only: [:id, :body]) }
+    comments = feature.comments.map { |comment| comment.as_json(only: [:id, :body ,:earthquake_id]) }
     comments
   end
 
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost', '127.0.0.1'
+    origins 'localhost:4200', '127.0.0.1:4200'
     resource '/api/*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options]
